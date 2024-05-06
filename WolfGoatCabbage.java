3bafc38e98f1b83073d118d38c8bc9e4a9b5aad3
Asatillo02 <106449309+Asatillo02@users.noreply.github.com>
2024-05-06 15:03:09 -0400

    Update WolfGoatCabbage.java

--- WolfGoatCabbage.java
+++ WolfGoatCabbage.java
@@ -4,7 +4,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.CompoundBorder;
+import java.awt.FlowLayout;
 
 public class WolfGoatCabbage extends JFrame {
 
@@ -34,9 +40,37 @@ public class WolfGoatCabbage extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new TitledBorder(new CompoundBorder(), "Wolf, Goat and Cabbage Puzzle", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton Resgin = new JButton("Resign");
+		Resgin.setBackground(new Color(255, 0, 0));
+		Resgin.setVerticalAlignment(SwingConstants.BOTTOM);
+		Resgin.setForeground(new Color(0, 0, 0));
+		Resgin.setToolTipText("Click the button to see solution!");
+		Resgin.setFont(new Font("Times New Roman", Font.ITALIC, 14));
+		Resgin.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(Resgin);
+		
+		JButton btnNewButton = new JButton("Goat");
+		btnNewButton.setFont(new Font("Times New Roman", Font.ITALIC, 14));
+		btnNewButton.setToolTipText("Do not leave him alone with Cabbage");
+		btnNewButton.setBackground(new Color(128, 128, 64));
+		contentPane.add(btnNewButton);
+		
+		JButton Wolf = new JButton("Wolf");
+		Wolf.setToolTipText("Do not leave him alone with the Goat\r\n");
+		Wolf.setBackground(new Color(192, 192, 192));
+		Wolf.setHorizontalAlignment(SwingConstants.LEADING);
+		Wolf.setFont(new Font("Times New Roman", Font.ITALIC, 14));
+		contentPane.add(Wolf);
+		
+		JButton Cabbage = new JButton("Cabbage");
+		Cabbage.setBackground(new Color(128, 255, 0));
+		Cabbage.setFont(new Font("Times New Roman", Font.ITALIC, 14));
+		contentPane.add(Cabbage);
 		
 	}
 
